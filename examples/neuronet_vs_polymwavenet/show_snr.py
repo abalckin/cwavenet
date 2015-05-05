d80b20511c27997fc29d8d8844f0e398392cd0fe
Verzunov Sergei <abalckin@hotmail.com>
2015-05-06 00:56:06 +0600

    WNvsNN example modifed

--- examples/neuronet_vs_polymwavenet/show_snr.py
+++ examples/neuronet_vs_polymwavenet/show_snr.py
@@ -8,7 +8,7 @@ rc('text.latex', unicode=True)
 rc('text.latex', preamble=r'\usepackage[russian]{babel}')
 #rc('font',**{'family':'serif'})
 rc('font',**{'size':'21'})
-res = np.loadtxt('result.txt', delimiter=', ')[0:8]
+res = np.loadtxt('result.txt', delimiter=', ')[0:10]
 #import pdb; pdb.set_trace()
 #plb.barh(y_pos, performance, xerr=error, align='center', alpha=0.4)
 #plb.yscale('linear')
@@ -20,13 +20,13 @@ plb.fill_between(res[:, 1], res[:, 1], res[:, 1]-np.max(res[:, 1]), res[:, 1], a
 plb.xscale('log')
 plb.legend(loc=0)
 plb.xlim(res[-1, 1]-0.1, res[0, 1]+50)
-plb.ylim(0, 1500)
+plb.ylim(0, 2100)
 plb.gca().set_xticks(res[:, 1])
 #plb.gca().xaxis.set_major_locator(plb.LogLocator(numticks=50))
 plb.gca().xaxis.set_major_formatter(plb.ScalarFormatter())
 plb.ylabel('Отношение сигнал/шум на выходе сети, M')
 plb.xlabel('Отношение сигнал/шум на входе сети, S')
-plb.annotate('Область применимости нейронных сетей', [13, 700])
+#plb.annotate('Область применимости нейронных сетей', [13, 700])
 plb.show()
 a = 3
 b = -3
@@ -36,7 +36,7 @@ neur_min = res[a:b, 11]-res[a:b, 12]
 neur_max = res[a:b, 11]+res[a:b, 12]
 poly_min = res[a:b, 5]-res[a:b, 6]
 poly_max = res[a:b, 5]+res[a:b, 6]
-#import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 print('Улучшение в среднем на {}%'.format(np.average((poly_avg-neur_avg)/neur_avg*100)))
 print('Улучшение в по диапазону на {0}-{1}%'.format(np.average((poly_min-neur_min)/neur_min*100),
       np.average((poly_max-neur_max)/neur_max*100)))
