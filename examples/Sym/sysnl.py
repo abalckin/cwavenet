c3df7c208a07b84609fc4791a69d85c8e8441cd5
Verzunov Sergei <abalckin@hotmail.com>
2015-05-22 00:19:59 +0600

    Add final example

--- examples/Sym/sysnl.py
+++ examples/Sym/sysnl.py
@@ -12,7 +12,7 @@ import multiprocessing
 def func1(y, t):
     n = len(y)
     if n<t:
-        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u1(n)+np.random.normal(0., k)+1.)
+        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u1(n)+np.random.normal(0., ks)+1.)
         return func1(y, t)
     else:
         return(y)
@@ -22,30 +22,30 @@ def func1(y, t):
 def func2(y, t):
     n = len(y)
     if n<t:
-        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u2(n)+np.random.normal(0., k)+1.)
-        return func1(y, t)
+        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u2(n)+np.random.normal(0., ks)+1.)
+        return func2(y, t)
     else:
         return(y)
 
 def func3(y, t):
     n = len(y)
     if n<t:
-        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u3(n)+np.random.normal(0., k)+1.)
-        return func1(y, t)
+        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u3(n)+np.random.normal(0., ks)+1.)
+        return func3(y, t)
     else:
         return(y)
 
 def u1(t):
-    return 0.7*np.sin(np.pi*1.95*t/50+0.1)+1
+    return 1.
 
 def u2(t):
-    return 1.2*np.sin(np.pi*2.05*t/50-0.1)+1
+    return 1.5*np.sin(np.pi*3*t/50+1)+1
 
 def u3(t):
     return np.sin(np.pi*2*t/50)+1
 
 cpu_count = multiprocessing.cpu_count()
-N = 50
+N = 500
 np.random.seed()
 c0 = 0.0
 a0 = 148.5
@@ -58,27 +58,28 @@ nc = 12
 fcount = 2
 f0 = 0.
 fb = 1.
-T=100
-k=0.1
+T=150
+ky=0.1
+ks=0.02
+ku=0.02
 t = np.arange(0, T, 1.)
-eps = np.random.normal(0., k, t.shape[-1])
 inp1 = np.vectorize(u1)(t)
 inp2 = np.vectorize(u2)(t)
 inp3 = np.vectorize(u3)(t)
 sys1 = func1([.0, .0], T)
 sys2 = func2([.0, .0], T)
 sys3 = func3([.0, .0], T)
-eps = np.random.normal(0., k, t.shape[-1])
+eps = np.random.normal(0., ku, t.shape[-1])
 inp1= inp1+eps
-eps = np.random.normal(0., k, t.shape[-1])
+eps = np.random.normal(0., ku, t.shape[-1])
 inp2 = inp2+eps
-eps = np.random.normal(0., k, t.shape[-1])
+eps = np.random.normal(0., ku, t.shape[-1])
 inp3 = inp3+eps
-eps = np.random.normal(0., k, t.shape[-1])
+eps = np.random.normal(0., ky, t.shape[-1])
 tar1 = sys1+eps
-eps = np.random.normal(0., k, t.shape[-1])
+eps = np.random.normal(0., ky, t.shape[-1])
 tar2 = sys2+eps
-eps = np.random.normal(0., k, t.shape[-1])
+eps = np.random.normal(0., ky, t.shape[-1])
 tar3 = sys3+eps
 
 w = wn.Net(nc, c0,
@@ -96,7 +97,7 @@ time = np.ma.concatenate([t, t])
 inputs = np.ma.concatenate([inp1, inp2])
 targets = np.ma.concatenate([tar1, tar2])
 
-track = w.train(time, inputs, targets, wn.TrainStrategy.Gradient, N, 0., 1, True, False)
+track = w.train(time, inputs, targets, wn.TrainStrategy.Gradient, N, 0., 1, True, True)
 
 #ans = w.sim(t, inpc)
 #import pdb; pdb.set_trace()
