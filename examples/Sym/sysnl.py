19225c7bfbb86060ec0fae4ff77527e8471a97e4
Verzunov Sergei <abalckin@hotmail.com>
2015-05-23 16:51:46 +0600

    begin

--- examples/Sym/sysnl.py
+++ examples/Sym/sysnl.py
@@ -9,37 +9,12 @@ import multiprocessing
 # Mодельная система
 
 
-def func1(y, t):
+def func1(y, t, u):
+    mu = 40.
     n = len(y)
     if n<t:
-        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u1(n)+np.random.normal(0., ks)+1.)
-        return func1(y, t)
-    else:
-        return(y)
-
-
-        
-def func2(y, t):
-    n = len(y)
-    if n<t:
-        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u2(n)+np.random.normal(0., ks)+1.)
-        return func2(y, t)
-    else:
-        return(y)
-
-def func3(y, t):
-    n = len(y)
-    if n<t:
-        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u3(n)+np.random.normal(0., ks)+1.)
-        return func3(y, t)
-    else:
-        return(y)
-
-def func4(y, t):
-    n = len(y)
-    if n<t:
-        y.append(y[-1]*y[-2]/(1+y[-1]**2+y[-2]**2)+u4(n)+np.random.normal(0., ks)+1.)
-        return func4(y, t)
+        y.append((y[-1]*y[-2])*y[-1]/(1.+2*y[-1]**2+y[-2]**2)+u(n)-np.random.normal(0., ks))
+        return func1(y, t, u)
     else:
         return(y)
 
@@ -68,21 +43,22 @@ p1 = 1.
 nc = 12
 fcount = 0
 f0 = 0.
-fb = 1.
-T=150
-ky=0.1
-ks=0.02
-ku=0.02
+fb = 2
+T=50
+ky=1.e-10
+ks=1.e-10
+ku=1.e-10
 t = np.arange(0, T, 1.)
 inp1 = np.vectorize(u1)(t)
 inp2 = np.vectorize(u2)(t)
 inp3 = np.vectorize(u3)(t)
 inp4 = np.vectorize(u4)(t)
-
-sys1 = func1([.0, .0], T)
-sys2 = func2([.0, .0], T)
-sys3 = func3([.0, .0], T)
-sys4 = func4([.0, .0], T)
+y0=2.4
+y1=2.4
+sys1 = func1([y0, y1], T, u1)
+sys2 = func1([y0, y1], T, u2)
+sys3 = func1([y0, y1], T, u3)
+sys4 = func1([y0, y1], T, u4)
 
 eps = np.random.normal(0., ku, t.shape[-1])
 inp1= inp1+eps
@@ -113,11 +89,11 @@ time = np.ma.concatenate([t, t])
 inputs = np.ma.concatenate([inp1, inp2])
 targets = np.ma.concatenate([tar1, tar2])
 
-track = w.train(time, inputs, targets, wn.TrainStrategy.Gradient, N, 0., 1, True, True)
-#track = w.train(t, inp1, tar1, wn.TrainStrategy.Gradient, N, 0., 1, True, True)
+#track = w.train(time, inputs, targets, wn.TrainStrategy.Gradient, N, 0., 1, True, True)
+track = w.train(t, inp2, tar2, wn.TrainStrategy.Gradient, N, 0., 1, True, True)
 #ans = w.sim(t, inpc)
 #import pdb; pdb.set_trace()
-tool.plot(t, inp3, w, track, orig=sys3, target=tar3)
+tool.plot(t, inp2, w, track, orig=sys2, target=tar2)
 plb.show()
 #plb.plot(t, sys2, t, w.sim(t, inp2))
 #plb.show()
